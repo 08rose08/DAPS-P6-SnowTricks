c107ce9df1069f46774d554b6e7bf55ea2b90be9
08rose08 <naudinrose@gmail.com>
2020-11-12 16:58:14 +0100

    Add comment + crud + fixture + include in template: trickShow

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\Trick;
 use App\Form\Trick1Type;
 use App\Repository\TrickRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CommentRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class TrickController extends AbstractController
@@ -49,8 +50,8 @@ class TrickController extends AbstractController
     /**
      * @Route("trick/{id}", name="trick_show", methods={"GET"})
      */
-    public function show(Trick $trick): Response
-    {
+    public function show(Trick $trick, CommentRepository $commentRepository): Response
+    {   
         return $this->render('trick/show.html.twig', [
             'trick' => $trick,
         ]);
